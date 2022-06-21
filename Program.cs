77e67aada95c9a156404d88a0a8ac5522de01ba4
Duy Linh <cudotn111@gmail.com>
2022-06-21 21:51:59 +0700

    Duy Linh

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿class Exercise4
+{
+    public static void Main(String[] args)
+    {
+        do
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i < 10; i++)
+            {
+                Console.WriteLine("{0} * {1} = {2}", num, i, num * i);
+            }
+        } while (false);
+    }
+}
